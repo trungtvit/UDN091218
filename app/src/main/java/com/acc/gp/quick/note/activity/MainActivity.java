d24d1e8e2392867323eaa0bb84d988c4a0933a52
TrungDK <trungtv@steadfast-inc.com>
2018-12-09 15:41:52 +0700

    CreateProject

---
+++ app/src/main/java/com/acc/gp/quick/note/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.acc.gp.quick.note.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.acc.gp.quick.note.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
