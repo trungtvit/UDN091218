7ffa3054aee149e8bb9694164f3a29250ef539ec
TrungDK <trungtv@steadfast-inc.com>
2018-12-10 22:35:10 +0700

    UPdate

--- app/src/main/java/com/acc/gp/quick/note/activity/MainActivity.java
+++ app/src/main/java/com/acc/gp/quick/note/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package com.acc.gp.quick.note.activity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.acc.gp.quick.note.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 
+        initUI();
+
+    }
+
+    private void initUI() {
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                Intent intentCreateNote = new Intent(MainActivity.this, CreateNoteActivity.class);
+                startActivity(intentCreateNote);
+                break;
+        }
     }
 }
